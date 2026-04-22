5a7b742b383ce6ae71ca9e2093b5abf93a91b1c7
tvquan2004-svg <tvquan2004@gmail.com>
2026-04-22 17:23:27 +0700

    update config

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,5 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 export default defineConfig({
-    base: "/My-Portfolio/",
-    plugins: [react()],
-    resolve: {
-        alias: {
-            "@": fileURLToPath(new URL("./src", import.meta.url)),
-        },
-    },
-});
+  plugins: [react()],
+})
